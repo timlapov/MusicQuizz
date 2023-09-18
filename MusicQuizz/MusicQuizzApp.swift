71943287b4b6a9d8d65023b78c3788da4efc49b2
Artem LAPOV <artemlapov@me.com>
2023-09-18 16:14:43 +0200

    Initial Commit

---
+++ MusicQuizz/MusicQuizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicQuizzApp.swift
+//  MusicQuizz
+//
+//  Created by Artem Lapov on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicQuizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
